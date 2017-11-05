c80bb66f14c08c09ed448dc2bd9f8cf3772c7867
raviguru007 <raviguru@goparties.com>
2017-11-05 14:51:36 +0530

    enhance task 2

--- task2/index.js
+++ task2/index.js
@@ -1,32 +1,50 @@
 (function () {
 
-	
+
 
 
 	function init() {
 
 		var canvas = document.getElementsByTagName('canvas')[0];
 		var c = canvas.getContext('2d');
-
 		var radius = 25;
         var s = 50; //square edge
         var container = { x: 0, y: 0, width: 900, height: 700 };
-        var circles = [
-        { x: getRandX(), y: getRandY(), r: radius, color: getRandomInt(30,330), vx: getRandVel(), vy: getRandVel() },
-        { x: getRandX(), y: getRandY(), r: radius, color: getRandomInt(30,330), vx: getRandVel(), vy: getRandVel() },
-        { x: getRandX(), y: getRandY(), r: radius, color: getRandomInt(30,330), vx: getRandVel(), vy: getRandVel() },
-        { x: getRandX(), y: getRandY(), r: radius, color: getRandomInt(30,330), vx: getRandVel(), vy: getRandVel() },
-        { x: getRandX(), y: getRandY(), r: radius, color: getRandomInt(30,330), vx: getRandVel(), vy: getRandVel() }
-
+        var Number_of_ball=getRandomInt(1,15);
         
 
+        function check_balls_collision(balls,ball){
+        	for(var i=0;i<balls.length;i++){
+        		var dx = balls[i].x - ball.x;	
+        		var dy = balls[i].y - ball.y;
+        		var distance = Math.sqrt(dx * dx + dy * dy);
+        		if (distance< balls[i].r + ball.r){
+        			return true;
+        		}
 
-        ];
+        	}
+        	return false;
+        }
+
+        
+        function produce_number_of_ball(ball_count){
+        	var balls=[];
+        	while(balls.length<ball_count){
+        		var ball={ x: getRandX(), y: getRandY(), r: radius, color: getRandomInt(30,330), vx: getRandVel(), vy: getRandVel() };
+        		if(!check_balls_collision(balls,ball)){
+        			balls.push(ball);
+        		}
+        	}
+        	return balls;
+        }
+
+
+        var circles=produce_number_of_ball(Number_of_ball);
         var count = circles.length;
 
         function getRandVel (){
-        	var min = 11;
-        	var max = 19;
+        	var min = 5;
+        	var max = 10;
         	var vel = getRandomInt(min, max);
         	vel = Math.random() <0.5 ? -vel : vel;
 
@@ -63,23 +81,18 @@
         }
 
         function draw() {
-
-        	c.fillStyle = '#bef8cc';
+        	c.fillStyle = 'black';
         	c.strokeStyle = 'black';
         	c.fillRect(container.x, container.y, container.width, container.height);
-            //c.clearRect(container.x,container.y,container.width,container.height);
-            //c.strokeRect(container.x,container.y,container.width,container.height);
+        	score();
 
-            score();
-
-            for (var i = 0; i < circles.length; i++) {
-            	c.fillStyle = 'hsl(' + circles[i].color + ',100%,50%)';
-            	c.beginPath();
-            	c.arc(circles[i].x, circles[i].y, circles[i].r, 0, 2 * Math.PI, false);
-            	c.fill();
+        	for (var i = 0; i < circles.length; i++) {
+        		c.fillStyle = 'hsl(' + circles[i].color + ',100%,50%)';
+        		c.beginPath();
+        		c.arc(circles[i].x, circles[i].y, circles[i].r, 0, 2 * Math.PI, false);
+        		c.fill();
 
                 //check if the ball has hit any of the container's walls
-
                 if ((circles[i].x + circles[i].vx + circles[i].r > container.x + container.width) || (circles[i].x - circles[i].r + circles[i].vx < container.x)) {
                 	circles[i].vx = - circles[i].vx;
                 }
@@ -106,44 +119,41 @@
 
                 for (j = 0; j < circles.length; j++) {
                 	if (j != i) {
-                		var dx = circles[i].x - circles[j].x;
-                		var dy = circles[i].y - circles[j].y;
-                		var distance = Math.sqrt(dx * dx + dy * dy);
+                		if (check_balls_collision([circles[i]],circles[j])) {
+                			var vel_x1 = circles[i].vx;
+                			var vel_y1 = circles[i].vy;
+                			circles[i].vx = circles[j].vx;
+                			circles[i].vy = circles[j].vy;
+                			circles[j].vx = vel_x1;
+                			circles[j].vy = vel_y1;
+                			//after balls collision,check if the ball has hit any of the container's walls
+                			if ((circles[i].x + circles[i].vx + circles[i].r > container.x + container.width) || (circles[i].x - circles[i].r + circles[i].vx < container.x)) {
+                				circles[i].vx = - circles[i].vx;
+                			}
+                			if ((circles[i].y + circles[i].vy + circles[i].r > container.y + container.height) || (circles[i].y - circles[i].r + circles[i].vy < container.y)) {
+                				circles[i].vy = - circles[i].vy;
+                			}
 
-                		if (distance < circles[i].r + circles[j].r) {
+                			//after balls collision,check if the ball has hit any of the container's walls
+                			if ((circles[j].x + circles[j].vx + circles[j].r > container.x + container.width) || (circles[j].x - circles[j].r + circles[j].vx < container.x)) {
+                				circles[j].vx = - circles[j].vx;
+                			}
 
-                            // circles[i].vx = - circles[i].vx;
-                            // circles[i].vy = - circles[i].vy;
-                            // circles[j].vx = - circles[j].vx;
-                            // circles[j].vy = - circles[j].vy;
-                            var vel_x1 = circles[i].vx;
-                            var vel_y1 = circles[i].vy;
-
-                            circles[i].vx = circles[j].vx;
-                            circles[i].vy = circles[j].vy;
-
-                            circles[j].vx = vel_x1;
-                            circles[j].vy = vel_y1;
-
-
-                        }
-                    }
+                			if ((circles[j].y + circles[j].vy + circles[j].r > container.y + container.height) || (circles[j].y - circles[j].r + circles[j].vy < container.y)) {
+                				circles[j].vy = - circles[j].vy;
+                			}
+                		}
+                	}
                 }
 
                 circles[i].x += circles[i].vx;
                 circles[i].y += circles[i].vy;
             }
 
-
-
             requestAnimationFrame(draw);
 
         }
-
-
         requestAnimationFrame(draw);
-
-
     }
 
     //invoke function init once document is fully loaded
